5ca6093b0aa29978a5f47bb4b29c520b05a17c56
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2025-05-20 13:52:23 +0530

    first spring boot example

---
+++ first/src/main/java/com/example/demo/controller/NavController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController {
+	
+	@GetMapping("/java")
+	public String openJava() {
+		return "java";
+	}
+	
+	@GetMapping("/python")
+	public String openPython() {
+		return "python";
+	}
+	
+	@GetMapping("/sql")
+	public String openSql() {
+		return "sql";
+	}
+
+}
